028510bd8325dffeebba0173be80a58adca3126e
Amar H. Pujari. <amar.pujari28@gmail.com>
2024-05-11 23:31:13 +0530

    Todays Dsa Practice with LinkedList

---
+++ src/dsa/linkedlist/models/Node.java
@@ -0,0 +1,13 @@
+package dsa.linkedlist.models;
+
+public class Node {
+	public int data;
+	public Node next;
+
+	public Node() {}
+	public Node(int data, Node next) {
+		super();
+		this.data = data;
+		this.next = next;
+	}
+}
